07634419e7b0b1b9756d4209e2eb6a47ffa66121
Silviomamani <silvio_argentina_96@Hotmail.com>
2026-08-26 09:58:56 -0300

    mod auth

--- app/src/main/java/com/silviomamani/pexelsapp/network/AuthInterceptor.kt
+++ app/src/main/java/com/silviomamani/pexelsapp/network/AuthInterceptor.kt
@@ -7,7 +7,7 @@ import okhttp3.Response
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "xLXT6Z2TgweUUdaFx6fUdpaEoSR0mjfydXGqrqpOWkcvI7RzKdMCZK1r") // 🔒 Reemplaza con tu clave real
+            .addHeader("Authorization", "xLXT6Z2TgweUUdaFx6fUdpaEoSR0mjfydXGqrqpOWkcvI7RzKdMCZK1r")
             .build()
         return chain.proceed(request)
     }
